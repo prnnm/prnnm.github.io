e458ffbed57c97e985f771baee884988e1d8b7fe
prnnm <97754563+prnnm@users.noreply.github.com>
2022-01-16 15:04:01 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+    const said = e => {
+        const value = e.value;
+        e.value = value.replace(/\D/g, '');
+    }
+
+    function save() {
+        var klv = document.getElementById("inp1").value;
+        var klv1 = document.getElementById("inp2").value;
+        var klv2 = document.getElementById("inp3").value;
+        var klv3 = document.getElementById("inp4").value;
+        var klv4 = document.getElementById("inp5").value;
+        
+        var a = pvi * klv;
+        var b = vs * klv;
+        var c = cna * klv;
+
+        var a1 = pvi1 * klv1;
+        var b1 = vs1 * klv1;
+        var c1 = cna1 * klv1;
+
+        var a2 = pvi2 * klv2;
+        var b2 = vs2 * klv2;
+        var c2 = cna2 * klv2;
+
+        var a3 = pvi3 * klv3;
+        var b3 = vs3 * klv3;
+        var c3 = cna3 * klv3;
+
+        var a4 = pvi4 * klv4;
+        var b4 = vs4 * klv4;
+        var c4 = cna4 * klv4;
+
+        var a5 = a + a1 + a2 + a3 + a4;
+        var b5 = b + b1 + b2 + b3 + b4;
+        var c5 = c + c1 + c2 + c3 + c4;
+
+        var no = b5.toString();
+        var no1= c5.toString();
+
+        console.log(no + no1)
+
+        var td1 = document.getElementById("td1").innerHTML = a5;
+        var td2 = document.getElementById("td2").innerHTML = no;
+        var td3 = document.getElementById("td3").innerHTML = no1;
+    }
+
+    var pvi = 55000;
+    var vs = 1900;
+    var cna = 99.54;
+
+    var pvi1 = 7500;
+    var vs1 = 0.265;
+    var cna1 = 17.81;
+
+    var pvi2 = 1500;
+    var vs2 = 0.700;
+    var cna2 = 14.50;
+
+    var pvi3 = 9000;
+    var vs3 = 0.120;
+    var cna3 = 19.12;
+
+    var pvi4 = 3000
+    var vs4 = 1.2;
+    var cna4 = 22.58;
